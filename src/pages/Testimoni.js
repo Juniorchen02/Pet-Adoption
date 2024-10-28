d9f349c260074e0be4efe3673ac77fb1817f172d
Alfin Tan <alfin6010@gmail.com>
2024-10-28 17:13:13 +0700

    Change description and fixing

--- src/pages/Testimoni.js
+++ src/pages/Testimoni.js
@@ -10,25 +10,25 @@ const testimonials = [
   },
   {
     name: 'Ricky',
-    job: 'Web Developer',
+    job: 'Teacher',
     image: '/images/client/img2.jpg',
     text: 'Personally, this is the best app ever especially when it comes to adoption pet, booyah!',
   },
   {
     name: 'Rikka',
-    job: 'Web Developer',
+    job: 'Freelance',
     image: '/images/client/img3.jpg',
     text: "It's not just about pet, but also about education and some other service that make this app were different!",
   },
   {
     name: 'Dono',
-    job: 'Web Developer',
+    job: 'Athlete',
     image: '/images/client/img4.jpg',
     text: 'Easy to process, fast to get, Are the best words that i can say so far about this adoption app!',
   },
   {
     name: 'Blanche',
-    job: 'Web Developer',
+    job: 'Dentist',
     image: '/images/client/img5.jpg',
     text: 'Unlike the other adoption app, this app provides high quality of their services. Higly recommend this app.',
   },
@@ -45,45 +45,50 @@ const TestimonialSlider = () => {
         el: '.testimonial-pagination',
         clickable: true,
       },
+      autoplay: {
+        delay: 2000,
+      },
     });
   }, []);
 
   return (
-    <section className="testimonial-section">
-      <div className="testimonial-wrapper">
-        <div className="testimonial-header">
-          <h2 className="testimonial-title">What our clients say</h2>
-        </div>
-        <div className="testimonial-content">
-          <div className="swiper testimonial-slider">
-            <div className="swiper-wrapper">
-              {testimonials.map((testimonial, index) => (
-                <div className="swiper-slide testimonial-item" key={index}>
-                  <div className="testimonial-info">
-                    <img
-                      src={testimonial.image}
-                      alt={testimonial.name}
-                      className="testimonial-img"
-                    />
-                    <div className="testimonial-name-job">
-                      <h3 className="testimonial-name">{testimonial.name}</h3>
-                      <span className="testimonial-job">{testimonial.job}</span>
+    <div className="testimonial-background">
+      <section className="testimonial-section">
+        <div className="testimonial-wrapper">
+          <div className="testimonial-header">
+            <h2 className="testimonial-title">What our clients say</h2>
+          </div>
+          <div className="testimonial-content">
+            <div className="swiper testimonial-slider">
+              <div className="swiper-wrapper">
+                {testimonials.map((testimonial, index) => (
+                  <div className="swiper-slide testimonial-item" key={index}>
+                    <div className="testimonial-info">
+                      <img
+                        src={testimonial.image}
+                        alt={testimonial.name}
+                        className="testimonial-img"
+                      />
+                      <div className="testimonial-name-job">
+                        <h3 className="testimonial-name">{testimonial.name}</h3>
+                        <span className="testimonial-job">{testimonial.job}</span>
+                      </div>
+                    </div>
+                    <p className="testimonial-text">{testimonial.text}</p>
+                    <div className="testimonial-rating">
+                      {[...Array(5)].map((_, starIndex) => (
+                        <i key={starIndex} className="fas fa-star testimonial-star"></i>
+                      ))}
                     </div>
                   </div>
-                  <p className="testimonial-text">{testimonial.text}</p>
-                  <div className="testimonial-rating">
-                    {[...Array(5)].map((_, starIndex) => (
-                      <i key={starIndex} className="fas fa-star testimonial-star"></i>
-                    ))}
-                  </div>
-                </div>
-              ))}
+                ))}
+              </div>
+              <div className="testimonial-pagination swiper-pagination"></div>
             </div>
-            <div className="testimonial-pagination swiper-pagination"></div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
