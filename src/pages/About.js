9fb32d7c3a33ccd41b9e467a0a050a12ff584383
Alfin Tan <alfin6010@gmail.com>
2024-10-28 12:16:25 +0700

    make about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,57 @@
 import React from 'react';
+import '../styles/about.css';
+import TestimonialSlider from './Testimoni';
 
-const AboutUs = () => {
+const About = () => {
   return (
-    <div>
-      This is About
-    </div>
-  )
-}
+    <section className="section__container about__container" id="about">
+      <p className="section__subheader"></p>
+      <h2 className="section__header">About Us</h2>
 
-export default AboutUs;
+      <div className="about__row">
+        <div className="about__image">
+          <img className="about-img" src="/images/aboutus/about-1.jpg" alt="about" />
+        </div>
+        <div className="about__content">
+        <span><img src="/images/icon/about-1-icon.png" alt="about-icon" /></span>
+          <h4>Help you get to find the most pet you love</h4>
+          <p>
+            Find your favourite here in our application, giving you tons list of pets, choose which one you love!
+          </p>
+        </div>
+      </div>
+
+      <div className="about__row">
+        <div className="about__image">
+          <img className="about-img" src="/images/aboutus/about-2.jpg" alt="about" />
+        </div>
+        <div className="about__content">
+        <span><img src="/images/icon/about-2-icon.png" alt="about-icon" /></span>
+          <h4>Take care of your pet. Just in your hands</h4>
+          <p>
+            Our staff will gave you solution according to your pets problem such as healthy things, giving you the best solution that you will ever get!.
+          </p>
+        </div>
+      </div>
+
+      <div className="about__row">
+        <div className="about__image">
+          <img className="about-img" src="/images/aboutus/about-3.jpg" alt="about" />
+        </div>
+        <div className="about__content">
+        <span><img src="/images/icon/about-3-icon.png" alt="about-icon" /></span>
+          <h4>Education and groom your precious pet</h4>
+          <p>
+           Beside adopt a pet, we also offer you to keep your pet save and healthy by our service, we help you to get some pet doctor for your pet and also education about pet.
+          </p>
+        </div>
+      </div>
+      
+      <TestimonialSlider />
+    </section>
+    
+    
+  );
+};
+
+export default About;
