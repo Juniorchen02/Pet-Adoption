19e729eed7c08766c325bd745ea0c49b2c7c1527
Alfin Tan <alfin6010@gmail.com>
2024-11-01 21:45:55 +0700

    Add backend and changes fro some files

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -10,16 +10,11 @@ import { verifyUser } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 
+// Detect and check the user
 router.get('/users', verifyUser, getUsers);
 router.get('/users/:id', verifyUser, getUserById);
 router.post('/users/', createUser);
 router.patch('/users/:id', verifyUser,  updateUser);
 router.delete('/users/:id', verifyUser, deleteUser);
-
-// router.get('/users', getUsers);
-// router.get('/users/:id', getUserById);
-// router.post('/users/', createUser);
-// router.patch('/users/:id',  updateUser);
-// router.delete('/users/:id', deleteUser);
 
 export default router;
