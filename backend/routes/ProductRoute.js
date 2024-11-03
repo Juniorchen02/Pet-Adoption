766d49928c9f2a1fb4eb2abb9273ed1781ef75e6
Alfin Tan <alfin6010@gmail.com>
2024-11-03 16:10:06 +0700

    Fixing typo on route

--- backend/routes/ProductRoute.js
+++ backend/routes/ProductRoute.js
@@ -13,6 +13,6 @@ router.get('/products', getProducts);
 router.get('/products/:id', getProductById);
 router.post('/products/', createProduct);
 router.patch('/products/:id', updateProduct);
-router.delete('./products/:id', deleteProduct);
+router.delete('/products/:id', deleteProduct);
 
 export default router;
