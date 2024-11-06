57549a346c43fa7e59d2e6194ab92c6b9eb8cd62
Juniorchen02 <chen.junior20@gmail.com>
2024-11-06 22:43:59 +0700

    update 06/11/2024 22:43

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from '../components/Header';
+import Gallery from './GalleryShow';
+import Service from './Service';
+import Team from './Team';
+import TestimonialSlider from './Testimoni';
+
+const Home = () => {
+    return (
+        <div className="App">
+            <Header />
+            <Gallery />
+            <Service />
+            <TestimonialSlider />
+            <Team />
+        </div>
+    );
+};
+
+export default Home;
