6a48566c52ea35079534fc48f2479bff247040e6
Juniorchen02 <chen.junior20@gmail.com>
2024-11-08 21:23:53 +0700

    docker build pertama

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,23 +1,23 @@
-// import { Sequelize } from "sequelize";
-
-// const db = new Sequelize(
-//     process.env.DB_NAME,           // Nama database
-//     process.env.DB_USER,           // Username
-//     process.env.DB_PASSWORD,       // Password
-//     {
-//         host: process.env.DB_HOST, // Host dari variabel lingkungan
-//         dialect: "mysql",
-//         port: process.env.DB_PORT || 3306 // Port (opsional, default ke 3306)
-//     }
-// );
- 
-// export default db;
-
 import { Sequelize } from "sequelize";
- 
-const db = new Sequelize('auth_db', 'root', '', {
-    host: "localhost",
-    dialect: "mysql"
-});
+
+const db = new Sequelize(
+    process.env.DB_NAME,           // Nama database
+    process.env.DB_USER,           // Username
+    process.env.DB_PASSWORD,       // Password
+    {
+        host: process.env.DB_HOST, // Host dari variabel lingkungan
+        dialect: "mysql",
+        port: process.env.DB_PORT || 3306 // Port (opsional, default ke 3306)
+    }
+);
  
 export default db;
+
+// import { Sequelize } from "sequelize";
+ 
+// const db = new Sequelize('auth_db', 'root', '', {
+//     host: "localhost",
+//     dialect: "mysql"
+// });
+ 
+// export default db;
